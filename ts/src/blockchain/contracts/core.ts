e6279765dc51c0cefa5ca1eecbb1365363827397
Mikhail Privalov <n5250@ya.ru>
2022-04-01 02:16:41 +0300

    CoreContract getUserPower

--- ts/src/blockchain/contracts/core.ts
+++ ts/src/blockchain/contracts/core.ts
@@ -2,9 +2,37 @@ import ReadApi from '../readApi'
 import { TableCodeConfig } from '../types'
 import BaseContract from './base'
 
+interface UserPowerData {
+  delegated: number
+  frozen: number
+  power: number
+  staked: number
+  username: string
+  with_badges: number
+}
+
 class CoreContract extends BaseContract {
   constructor(api: ReadApi, tableCodeConfig: TableCodeConfig) {
     super(api, tableCodeConfig, 'core')
+  }
+
+  async getUserPower(username: string, hostname: string) {
+    const powerData: UserPowerData | null = await this.getSingleTableRow<UserPowerData>({
+      table: 'power3',
+      scope: hostname,
+      lower_bound: username,
+      upper_bound: username,
+      limit: 1,
+    })
+
+    return powerData || {
+      delegated: 0,
+      frozen: 0,
+      power: 0,
+      staked: 0,
+      username,
+      with_badges: 0,
+    }
   }
 }
 
