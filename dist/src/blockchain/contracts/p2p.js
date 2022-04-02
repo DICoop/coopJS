757874dcaf05f0a711ed14f793acb3d846f7a4c3
Mikhail Privalov <n5250@ya.ru>
2022-04-02 17:43:55 +0300

    Beta 8

--- dist/src/blockchain/contracts/p2p.js
+++ dist/src/blockchain/contracts/p2p.js
@@ -8,24 +8,28 @@ class P2PContract extends base_1.default {
     constructor(api, tableCodeConfig) {
         super(api, tableCodeConfig, 'p2p');
     }
-    async getOrders(username, parent_id) {
+    async getOrders(username, parent_id, order_id) {
         const q = {
             table: 'orders',
             lower_bound: 0,
             limit: 100,
             getAllRows: true,
         };
-        if (username) {
+        if (typeof username !== 'undefined') {
             q.lower_bound = username;
             q.upper_bound = username;
             q.index_position = 5;
             q.key_type = 'i64';
         }
-        else if (parent_id) {
+        else if (typeof parent_id !== 'undefined') {
             q.lower_bound = parent_id;
             q.upper_bound = parent_id;
             q.index_position = 3;
             q.key_type = 'i64';
+        }
+        else if (typeof order_id !== 'undefined') {
+            q.lower_bound = order_id;
+            q.upper_bound = order_id;
         }
         const { rows } = await this.getTableRows(q);
         return rows.map(row => {
@@ -39,6 +43,10 @@ class P2PContract extends base_1.default {
             }
             return res;
         });
+    }
+    async getOrder(order_id) {
+        const [order] = await this.getOrders(undefined, undefined, order_id);
+        return order;
     }
     getUSDRates() {
         return this.getTableRows({
