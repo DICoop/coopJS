7307fc0c38aead3a90824c1f92d4fa6c259b8ec4
Mikhail Privalov <n5250@ya.ru>
2022-06-24 09:34:15 +0300

    dist

--- dist/src/blockchain/contracts/nft.d.ts
+++ dist/src/blockchain/contracts/nft.d.ts
@@ -14,11 +14,38 @@ export interface NftObject {
     title: string;
     total_pieces: number;
 }
+export interface DeliveryRequest {
+    type: string;
+    placeholder: string;
+}
+export interface NftMarketObject {
+    blocked_pieces: number;
+    buyer_can_offer_price: number;
+    delivery_from: string;
+    delivery_methods: string[];
+    delivery_operators: string[];
+    id: number;
+    is_auction: number;
+    lang: string;
+    meta: {
+        delivery_request: DeliveryRequest[];
+    };
+    object_id: number;
+    one_piece_price: string;
+    remain_pieces: number;
+    sales_closed_at: string;
+    sales_start_at: string;
+    seller: string;
+    status: string;
+    token_contract: string;
+    total_price: string;
+    with_delivery: number;
+}
 declare class NftContract extends BaseContract {
     constructor(api: ReadApi, tableCodeConfig: TableCodeConfig);
     getObjectsByOwner(owner: AccountName): Promise<NftObject[]>;
     getAllObjects(): Promise<NftObject[]>;
-    getMarket(): Promise<any[]>;
+    getMarket(): Promise<NftMarketObject[]>;
     fetchRequestsWithIndexPosition(username: AccountName, indexPosition: number): Promise<any[]>;
     fetchRequests(username: AccountName): Promise<any[]>;
 }
