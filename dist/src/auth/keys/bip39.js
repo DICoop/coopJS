4931fe0be6d20b957d7874a14f72ead9ba05b1df
Mikhail Privalov <n5250@ya.ru>
2022-06-26 18:42:23 +0300

    dist

--- dist/src/auth/keys/bip39.js
+++ dist/src/auth/keys/bip39.js
@@ -8,7 +8,7 @@ const bip39_1 = require("bip39");
 const english_json_1 = __importDefault(require("bip39/src/wordlists/english.json"));
 const isValidMnemonic = (mnemonic) => (0, bip39_1.validateMnemonic)(mnemonic, english_json_1.default);
 exports.isValidMnemonic = isValidMnemonic;
-const mnemonicToSeed = (mnemonic) => (0, bip39_1.mnemonicToSeed)(mnemonic, english_json_1.default).then((seed) => seed.toString('hex'));
+const mnemonicToSeed = (mnemonic) => (0, bip39_1.mnemonicToSeed)(mnemonic).then((seed) => seed.toString('hex'));
 exports.mnemonicToSeed = mnemonicToSeed;
 const generateMnemonic = () => (0, bip39_1.generateMnemonic)(null, null, english_json_1.default);
 exports.generateMnemonic = generateMnemonic;
