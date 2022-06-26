13566a806f0eb8874ac743888e981ce4fc7ee756
Mikhail Privalov <n5250@ya.ru>
2022-06-26 18:22:58 +0300

    dist

--- dist/src/auth/keys/bip39.js
+++ dist/src/auth/keys/bip39.js
@@ -1,10 +1,14 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.generateMnemonic = exports.mnemonicToSeed = exports.isValidMnemonic = void 0;
 const bip39_1 = require("bip39");
-const isValidMnemonic = (mnemonic) => (0, bip39_1.validateMnemonic)(mnemonic);
+const english_json_1 = __importDefault(require("bip39/src/wordlists/english.json"));
+const isValidMnemonic = (mnemonic) => (0, bip39_1.validateMnemonic)(mnemonic, english_json_1.default);
 exports.isValidMnemonic = isValidMnemonic;
-const mnemonicToSeed = (mnemonic) => (0, bip39_1.mnemonicToSeed)(mnemonic).then((seed) => seed.toString('hex'));
+const mnemonicToSeed = (mnemonic) => (0, bip39_1.mnemonicToSeed)(mnemonic, english_json_1.default).then((seed) => seed.toString('hex'));
 exports.mnemonicToSeed = mnemonicToSeed;
 const generateMnemonic = () => (0, bip39_1.generateMnemonic)();
 exports.generateMnemonic = generateMnemonic;
