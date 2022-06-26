6a18306042c0fd19f29fe66c7d09c5253095efa5
Mikhail Privalov <n5250@ya.ru>
2022-06-26 18:22:43 +0300

    Import lang

--- ts/src/auth/keys/bip39.ts
+++ ts/src/auth/keys/bip39.ts
@@ -1,8 +1,9 @@
 import { validateMnemonic, mnemonicToSeed as mnemonicToSeedBip39, generateMnemonic as gM } from 'bip39'
+import EN from 'bip39/src/wordlists/english.json'
 
-export const isValidMnemonic = (mnemonic: string) => validateMnemonic(mnemonic)
+export const isValidMnemonic = (mnemonic: string) => validateMnemonic(mnemonic, EN)
 
 export const mnemonicToSeed = (mnemonic: string) =>
-  mnemonicToSeedBip39(mnemonic).then((seed: Buffer) => seed.toString('hex'))
+  mnemonicToSeedBip39(mnemonic, EN).then((seed: Buffer) => seed.toString('hex'))
 
 export const generateMnemonic = (): string => gM()
