2c58abfcf580b92171faa08e047a96af57090150
Mikhail Privalov <n5250@ya.ru>
2022-08-02 16:20:43 +0300

    dist

--- dist/src/blockchain/contracts/nft.d.ts
+++ dist/src/blockchain/contracts/nft.d.ts
@@ -5,12 +5,11 @@ import BaseContract from './base';
 export interface NftObject {
     category: string;
     creator: string;
-    creator_can_emit_new_pieces: number;
     description: string;
     id: number;
     images: string[];
     ipns: string;
-    lang: string;
+    meta: Object;
     title: string;
     total_pieces: number;
 }
@@ -19,19 +18,16 @@ export interface DeliveryRequest {
     placeholder: string;
 }
 export interface NftMarketObject {
+    base_piece_price: string;
     blocked_pieces: number;
-    buyer_can_offer_price: number;
-    delivery_from: string;
-    delivery_methods: string[];
-    delivery_operators: string[];
+    day_finish: number;
+    day_start: number;
     id: number;
-    is_auction: number;
-    lang: string;
     meta: {
         delivery_request: DeliveryRequest[];
     };
+    min_piece_price: string;
     object_id: number;
-    one_piece_price: string;
     remain_pieces: number;
     sales_closed_at: string;
     sales_start_at: string;
@@ -39,7 +35,6 @@ export interface NftMarketObject {
     status: string;
     token_contract: string;
     total_price: string;
-    with_delivery: number;
 }
 declare class NftContract extends BaseContract {
     constructor(api: ReadApi, tableCodeConfig: TableCodeConfig);
